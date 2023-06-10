b64470aaa633e5b8e9b54e36a2779b695d79d016
Alex Lovy <Alex.lovy@gmail.com>
2023-06-10 20:27:43 +0300

    add tasks 64/66/68

---
+++ task_64/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+
+System.Console.Write("Введите число: ");
+
+int temp = Convert.ToInt32(Console.ReadLine());
+
+int number=1;
+
+void RoadToN(int number)
+{
+if (number == temp+1)
+{
+return;
+}
+RoadToN(number + 1);
+
+System.Console.Write($"{number} ");
+
+}
+
+
+RoadToN(number);
+
+
---
+++ task_66/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+System.Console.Write("Введите число: M ");
+int M = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите число: N");
+int N = Convert.ToInt32(Console.ReadLine());
+
+
+
+int sum = 0;
+
+void RoadToN(int M)
+
+{ 
+
+if (M == N+1)
+
+{
+System.Console.Write($"{sum} ");
+return;
+}
+sum = sum + M;
+
+RoadToN(M + 1);
+
+}
+
+
+
+RoadToN(M);
+
